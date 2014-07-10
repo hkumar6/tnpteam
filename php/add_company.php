0e70c6d349f16265da6664cc986c5fd539494b36
U-CIQHYD\kharsha <kharsha@H4T114.ciqhyd.com>
2014-07-10 18:33:19 +0530

    add_company page partially created

--- php/add_company.php
+++ php/add_company.php
@@ -1,4 +1,34 @@
 <?php
-
-
+	session_start();
+	if ( ! isset($_SESSION['user']))
+	{
+		header( 'Location: ../index.php' );
+	}
 ?>
+
+<!doctype html>
+<html>
+	<head>
+		<style type="text/css">
+			*{
+				font-family : "Helvetica Neue", Helvetica, Arial, sans-serif;
+			}
+			body{
+				font-size: 14px;
+				font-weight: bold;
+			}
+			#header{
+				width: 300px;
+				margin: auto;
+				text-align: center;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<div id="header">
+			<h1>TNP Team Portal</h1>
+			<hr />
+		</div>
+	</body>
+</html>
